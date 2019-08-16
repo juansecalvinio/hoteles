49feb85bd530ab9b62e867fd9930971c9ac31d5d
juansecalvinomtg <juansecalvinomtg@gmail.com>
2019-08-16 13:38:49 -0300

    Updates

---
+++ precache-manifest.21e86e59c264495c6b8d6708586731dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc8527cc30caff00105a001d78215fa",
+    "url": "/proyecto-hoteles/index.html"
+  },
+  {
+    "revision": "ed235432ff4a054ca8d1",
+    "url": "/proyecto-hoteles/static/css/2.f92e16c1.chunk.css"
+  },
+  {
+    "revision": "97e5f50a2e8ccadfda13",
+    "url": "/proyecto-hoteles/static/css/main.cca4fd7a.chunk.css"
+  },
+  {
+    "revision": "ed235432ff4a054ca8d1",
+    "url": "/proyecto-hoteles/static/js/2.85be9720.chunk.js"
+  },
+  {
+    "revision": "97e5f50a2e8ccadfda13",
+    "url": "/proyecto-hoteles/static/js/main.3bcd5915.chunk.js"
+  },
+  {
+    "revision": "8c91921aed27b168c2fc",
+    "url": "/proyecto-hoteles/static/js/runtime~main.c59bed12.js"
+  }
+]);
